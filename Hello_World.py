f316f8ea66e8cd25e5478e384c4c992e77bcf028
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-05-04 20:00:26 +0600

    create helloworld

---
+++ Hello_World.py
@@ -0,0 +1,10 @@
+# This program says hello and ask for your name
+print('hello World')
+print('What is your name?')    # Ask for the name
+myName = input()
+print('It is good to meet you.Mr.' +myName)
+print('The length of my name is:')
+print(len(myName))
+print('What is your age?')  # Ask for the age
+myAge=input()
+print('You will be' +str(int(myAge)+1) +'in a year')
