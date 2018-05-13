ae5e1e5ca547f107bdb1322a649dbc404f4b8235
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-05-13 15:41:11 +0600

    update pay

--- pay.py
+++ pay.py
@@ -1,11 +1,8 @@
 Hours=input('Enter Hours:')
-print(Hours)
 Rates=input('Enter Rates:')
-print(Hours)
 if int(Hours)>40:
-  pay=int(Hours)*int(Rates)+(int(Rates)*1.5)
+  pay=40*int(Rates)+(int(Hours)-40)*(int(Rates)*1.5)
   print(pay)
 else:
   pay=int(Hours)*int(Rates)
 print(pay)
-print(type(pay))
