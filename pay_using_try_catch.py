95b77a65764e604cb74660879f061bf2d04e0117
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-05-13 15:42:17 +0600

    Update pay_using_try_catch.py

--- pay_using_try_catch.py
+++ pay_using_try_catch.py
@@ -1,11 +1,11 @@
 try:
-  Hours=input('Enter Hours:')
-  Rates=input('Enter Rates:')
-  if int(Hours)>40:
-    pay=int(Hours)*int(Rates)+(int(Rates)*1.5)
-    print(pay)
-  else:
-    pay=int(Hours)*int(Rates)
-    print(pay)
+ Hours=input('Enter Hours:')
+Rates=input('Enter Rates:')
+if int(Hours)>40:
+  pay=40*int(Rates)+(int(Hours)-40)*(int(Rates)*1.5)
+  print(pay)
+else:
+  pay=int(Hours)*int(Rates)
+print(pay)
 except:
   print('Error,Please enter numeric input.')
