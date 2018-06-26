8a28c04ad59dffb48159057fa44dc7e0be4e5004
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-06-26 09:54:54 +0600

    Create Copy_files.py

---
+++ Copy_files.py
@@ -0,0 +1,4 @@
+import  openpyxl,shutil,os
+os.chdir('/home/mainul/Desktop/copy_files/automate_online-materials')
+shutil.copy('/home/mainul/PycharmProjects/Automate_The_Boring_stuff/practice projects/Chapter_12/chapter_11_exer.py','/home/mainul/Desktop/copy_files/automate_online-materials/chapter_11_all.py')
+print(os.getcwd())
