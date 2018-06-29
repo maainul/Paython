38a8eadcd7910a3a4965a24b975f63452e167cf2
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-06-30 01:50:49 +0600

    Create Working_with_docx_creat_doc_file_with_paragraph.py

---
+++ Working_with_docx_creat_doc_file_with_paragraph.py
@@ -0,0 +1,10 @@
+import Docx
+doc = docx.Documents()
+doc.add_paragraph('Hello world!', 'Title')
+doc.add_heading('Header 0', 0)
+doc.add_heading('Header 1', 1)
+doc.add_paragraph("Hello World")
+paraObj1 = doc.add_paragraph('This is a second paragraph.')
+paraObj2 = doc.add_paragraph('This is a yet another paragraph.')
+paraObj1.add_run(' This text is being added to the second paragraph.')
+doc.save('new.docx')
