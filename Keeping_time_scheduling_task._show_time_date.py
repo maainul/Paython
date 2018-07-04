af73b35a9b16103f4ec1a262ce0ea5ee412561a2
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-04 23:04:18 +0600

    Update Keeping_time_scheduling_task._show_time_date.py

--- Keeping_time_scheduling_task._show_time_date.py
+++ Keeping_time_scheduling_task._show_time_date.py
@@ -1,5 +1,14 @@
 import datetime
+import time
 
 dt = datetime.datetime.now()
 print(dt)
+
 print(dt.day, dt.month, dt.year, dt.hour, dt.minute, dt.second)
+
+ds = datetime.datetime.fromtimestamp(time.time())
+print(ds)
+
+td = datetime.timedelta(days=11, hours=5, minutes=12, seconds=8)
+print(td)
+
