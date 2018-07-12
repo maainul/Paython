804f65ab48b85b48bdba1fdb1954f10ee7fa55a4
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-12 13:10:09 +0600

    Create Total_sum_of_number_and_average_using_for_loop.py

---
+++ Total_sum_of_number_and_average_using_for_loop.py
@@ -0,0 +1,8 @@
+num = int(input('Enter  a number: '))
+sum = 0
+for i in range(0, num+1):
+    sum = sum + i
+    average = sum//num
+   # print(i)
+print('Sum of Number is:', sum)
+print('Average of number is: ', average)
