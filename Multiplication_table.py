7ca313af770b22e17b2f3b576651e74c1f963ab3
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-14 11:59:00 +0600

    Create Multiplication_table.py

---
+++ Multiplication_table.py
@@ -0,0 +1,23 @@
+n=int(input("Enter the number to print the tables for:"))
+for i in range(1,11):
+    print(n,"x",i,"=",n*i)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
