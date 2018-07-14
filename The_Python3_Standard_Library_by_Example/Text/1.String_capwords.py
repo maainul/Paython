0431e154d855ac4d9292ee2989a1a6453e04ce38
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-15 03:33:34 +0600

    Create 1.String_capwords.py

---
+++ The_Python3_Standard_Library_by_Example/Text/1.String_capwords.py
@@ -0,0 +1,8 @@
+import string 
+s = 'The quick brown Fox jumped over the lazy dog'
+print(s)
+print(string.capword(s))
+
+OUTPUT:
+The quick brown Fox jumped over the lazy dog
+The Quick Brown Fox Jumped Over The Lazy Dog
