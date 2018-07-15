43828ff5251967b307156808fbc8588018d85445
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-15 15:09:23 +0600

    Update and rename on.py to 1.string_capwords.py

---
+++ The_Python3_Standard_Library_by_Example/Chapter_1_Text/1.string_capwords.py
@@ -0,0 +1,8 @@
+import string
+s = 'The quick brown fox jumped over the lazy dog.'
+print(s)
+print(string.capwords(s))
+
+OUTPUT:
+The quick brown fox jumped over the lazy dog.
+The Quick Brown Fox Jumped Over The Lazy Dog.
