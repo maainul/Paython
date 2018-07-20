007c646ef3cbb0abfac84f323494226577a6fb1c
maainul <mainul080@yahoo.com>
2018-07-20 22:10:23 +0600

    date_time

---
+++ The_Python3_Standard_Library_by_Example/Chapters_Dates_and_times/4.24.datetime_format.py
@@ -0,0 +1,3 @@
+import datetime
+today = datetime.datetime.today()
+print('format(): {:%a %b %d %H:%M:%S %Y}'.format(today))
---
+++ The_Python3_Standard_Library_by_Example/Chapters_Dates_and_times/4.28.calender_textcalender.py
@@ -0,0 +1,3 @@
+import calendar
+c=calendar.TextCalendar(calendar.FRIDAY)
+print(c.prmonth(2017,7))
